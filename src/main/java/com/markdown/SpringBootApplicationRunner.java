84f4eb232c7ce2addcba85b62cbf49aa7054914a
ZhaoBO <zhaoboy@aliyun.com>
2020-05-26 14:01:46 +0800

    呵呵，试试IDEA自带的提交可视化，好不好用

--- src/main/java/com/markdown/SpringBootApplicationRunner.java
+++ src/main/java/com/markdown/SpringBootApplicationRunner.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching                  //注解代表使用redis缓存
+@EnableCaching //注解代表使用redis缓存
 public class SpringBootApplicationRunner {
 
     public static void main(String[] args) {
